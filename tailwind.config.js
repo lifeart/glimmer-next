c9938aa2bb8110341a037b3065e15dfbbd7c6d79
Alex Kanunnikov <lifeart92@gmail.com>
2024-01-09 16:09:55 +0300

    +

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,gjs,gts}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./index.html', './src/**/*.{js,ts,gjs,gts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
