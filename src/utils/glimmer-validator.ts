75d718ea6fd479a8f0858477065a322e370e12b4
Alex Kanunnikov <lifeart92@gmail.com>
2024-02-06 20:40:38 +0300

    glimmer-validator-compat

---
+++ src/utils/glimmer-validator.ts
@@ -0,0 +1,57 @@
+import { Cell, cellFor, cellsMap, getTracker } from './reactive';
+
+export { cellFor as tagFor } from '@lifeart/gxt';
+
+export function dirtyTagFor(obj: object, key: string | number | symbol): void {
+  // @ts-expect-error
+  const cell = cellFor(obj, key);
+  cell.update(cell.value);
+}
+export function tagMetaFor(obj: object): any {
+  return cellsMap.get(obj);
+}
+
+export function isTracking(): boolean {
+  return getTracker() !== null;
+}
+
+export function consumeTag(tag: Cell): void {
+  const TRACKER = getTracker();
+  if (TRACKER !== null) {
+    TRACKER.add(tag);
+  }
+}
+
+export type Getter<T, K extends keyof T> = (self: T) => T[K] | undefined;
+export type Setter<T, K extends keyof T> = (self: T, value: T[K]) => void;
+
+export function trackedData<T extends object, K extends keyof T>(
+  key: K,
+  initializer?: (this: T) => T[K],
+): { getter: Getter<T, K>; setter: Setter<T, K> } {
+  let values = new WeakMap<T, T[K]>();
+  let hasInitializer = typeof initializer === 'function';
+
+  function getter(self: T) {
+    consumeTag(cellFor(self, key));
+
+    let value;
+
+    // If the field has never been initialized, we should initialize it
+    if (hasInitializer && !values.has(self)) {
+      value = initializer!.call(self);
+      values.set(self, value);
+    } else {
+      value = values.get(self);
+    }
+
+    return value;
+  }
+
+  function setter(self: T, value: T[K]): void {
+    dirtyTagFor(self, key);
+    values.set(self, value);
+  }
+
+  return { getter, setter };
+}
