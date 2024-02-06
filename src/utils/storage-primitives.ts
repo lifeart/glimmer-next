d952930fcc3c77f217fc8bc1cd5c9ded8616217b
Alex Kanunnikov <lifeart92@gmail.com>
2024-02-06 16:46:18 +0300

    storage-primitives (#92)
    * storage-primitives
    
    * +
    
    * +

---
+++ src/utils/storage-primitives.ts
@@ -0,0 +1,27 @@
+import { cell, type Cell } from '@lifeart/gxt';
+import { isFn } from './shared';
+interface Storage<T> {
+  cell: Cell<T>;
+  update(value: T): void;
+}
+
+export function createStorage<T>(
+  initialValue?: T,
+  isEqual?: (oldValue: T, newValue: T) => boolean,
+): Storage<T> {
+  return {
+    cell: cell<T>(initialValue as T, 'storage-primitive'),
+    update(value: T) {
+      if (isFn(isEqual) && isEqual(this.cell.value as T, value)) {
+        return;
+      }
+      this.cell.update(value);
+    },
+  };
+}
+export function getValue<T>(storage: Storage<T>): T {
+  return storage.cell.value;
+}
+export function setValue<T>(storage: Storage<T>, value: T) {
+  storage.update(value);
+}
