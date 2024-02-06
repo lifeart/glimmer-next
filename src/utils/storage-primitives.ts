f3a002fa9d94cb34bf934e2d0f47b1b88de05c74
Alex Kanunnikov <lifeart92@gmail.com>
2024-02-06 16:31:03 +0300

    +

--- src/utils/storage-primitives.ts
+++ src/utils/storage-primitives.ts
@@ -1,11 +1,7 @@
 import { cell, type Cell } from '@lifeart/gxt';
-
-const CELL_SYMBOL = Symbol('cell');
-const CELL_VALUE = Symbol('cell-value');
-
+import { isFn } from './shared';
 interface Storage<T> {
-  [CELL_SYMBOL]: true;
-  [CELL_VALUE]: Cell<T>;
+  cell: Cell<T>;
   update(value: T): void;
 }
 
@@ -14,18 +10,17 @@ export function createStorage<T>(
   isEqual?: (oldValue: T, newValue: T) => boolean,
 ): Storage<T> {
   return {
-    [CELL_SYMBOL]: true,
-    [CELL_VALUE]: cell<T>(initialValue as T, 'storage-primitive'),
+    cell: cell<T>(initialValue as T, 'storage-primitive'),
     update(value: T) {
-      if (isEqual && isEqual(this[CELL_VALUE]?.value as T, value)) {
+      if (isFn(isEqual) && isEqual(this.cell.value as T, value)) {
         return;
       }
-      this[CELL_VALUE].update(value);
+      this.cell.update(value);
     },
   };
 }
 export function getValue<T>(storage: Storage<T>): T {
-  return (storage[CELL_VALUE]?.value ?? undefined) as unknown as T;
+  return storage.cell.value;
 }
 export function setValue<T>(storage: Storage<T>, value: T) {
   storage.update(value);
