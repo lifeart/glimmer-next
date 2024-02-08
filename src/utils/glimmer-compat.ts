c37726355bbea4305dfb194f198099c56badc8d5
Alex Kanunnikov <lifeart92@gmail.com>
2024-02-08 20:47:23 +0300

    owner primitives (#97)
    * owner
    
    * +

--- src/utils/glimmer-compat.ts
+++ src/utils/glimmer-compat.ts
@@ -3,3 +3,4 @@ export * as destroyable from './destroyable';
 export * as storage from './storage-primitives';
 export * as validator from './glimmer-validator';
 export * as reference from './glimmer-reference';
+export * as owner from './owner';
